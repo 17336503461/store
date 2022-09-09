47a40b45d5130597cf2f63d2297b98f027ac2aca
王昱璐 <2458651807@qq.com>
2022-09-09 20:50:08 +0800

    科目结业提交

--- src/views/repository/unitSubmit/index.vue
+++ src/views/repository/unitSubmit/index.vue
@@ -25,12 +25,13 @@
             </div>
             <hr>
             <div className="second">
-            <p>&nbsp;&nbsp;科目练习项目地址:</p>
+                <p>&nbsp;&nbsp;科目练习项目地址:</p>
                 &nbsp; &nbsp; &nbsp;
-                
-                <a-textarea placeholder="多行输入" style="width:400px;height:80px;margin-left: 130px;margin-top:-30px;" allow-clear/>
-                    <!--<input type="text" placeholder="请填写项目地址" style="width:400px;height:80px;text-align: left">-->
-            
+
+                <a-textarea placeholder="多行输入" style="width:400px;height:80px;margin-left: 130px;margin-top:-30px;"
+                    v-model="address" allow-clear />
+                <!--<input type="text" placeholder="请填写项目地址" style="width:400px;height:80px;text-align: left">-->
+
 
 
             </div>
@@ -38,7 +39,7 @@
 
                 &nbsp;&nbsp;对当前授课教师评分:
                 &nbsp;&nbsp;&nbsp;&nbsp;
-                <a-rate :default-value="0" allow-clear style="margin-bottom:-5px;"/>
+                <a-rate :default-value="0" allow-clear style="margin-bottom:-5px;" />
                 <!--<icon-star size='20px'/>
                 <icon-star size='20px'/>
                 <icon-star size='20px'/>
@@ -56,9 +57,15 @@
             </div>
             <div className="teacher-image">
                 <div className="image-left">&nbsp;&nbsp;对当前授课老师印象:</div>
-                <div className="image-right">
-                    技术够好&nbsp;&nbsp; 讲课细致&nbsp;&nbsp; 技术一般&nbsp;&nbsp; 讲课卡顿
-                    <p>总爱迟到&nbsp;&nbsp; 认真负责&nbsp;&nbsp; 不关心学员&nbsp;&nbsp; 课程互动多</p>
+                <div className="image-right" @click="teacher()">
+                    <a-space direction="vertical" size="large" fill>
+                        <!--<a-descriptions :data="data"  :align="{ label: 'right' }" />-->
+                        <a-descriptions :data="data"  :column="{xs:2, md:2, lg:4}">
+                        <a-descriptions-item v-for="item of data" :label="item.label" :align="{ label: 'right'}" >
+                            <a-tag>{{ item.value }}</a-tag>
+                        </a-descriptions-item>
+                    </a-descriptions>
+                    </a-space>
                 </div>
 
 
@@ -66,38 +73,82 @@
 
             <div className="bable">
                 &nbsp;&nbsp;&nbsp;其他印象标签(选填):&nbsp;&nbsp;&nbsp;&nbsp;
-                <a-input :style="{ width: '320px' }" placeholder="请填选" allow-clear />
+                <a-input :style="{ width: '320px' }" placeholder="请填选" allow-clear v-model="write" />
             </div>
-            <a-button type="outline" className="button">提交</a-button>
-           
+            <a-space>
+
+                <a-button type="outline" className="button">提交</a-button>
+            </a-space>
+
         </div>
     </div>
 </template>
 <script  lang="ts">
- //import postgetList from './views/unitSubmit.vue';//request.js还没写
+//import postgetList from './views/unitSubmit.vue';//request.js还没写
+//import {postgetList} from '';
 export default {
     name: 'UnitSubmit',
-   
-    data(){
-        return{
-            value:1//postgetList,
+    data() {
+        return {
+            techlist: [],//技术栈分类
+            subjectlist: [],//科目分类
+
+            address: '',
+            write: '',
+
 
         }
     },
-    methods(){
+    methods: {
+        teacher() {
 
+        }
     },
-    created(){
+    created() {
         //生命周期钩子
-    }
+    },
+    setup() {
+        const data = [{
+            value: '技术够好',
+        }, {
+
+            value: '讲课细致',
+        }, {
+
+            value: '技术一般'
+        }, {
+
+            value: '讲课卡顿',
+        },
+        {
+            value: '总爱迟到'
+        },
+        {
+
+            value: '认真负责'
+        },
+        {
+
+            value: '不关心学员'
+        },
+        {
+
+            value: '课程互动多'
+        }];
+
+        return {
+            data
+        }
+    },
 };
 </script>
 <style scoped lang="less">
-   .container{
+.container {
     background-color: #fff;
-    width:100%;
- 
-   }
+    width: 100%;
+
+}
+
 hr {
     width: 1020px;
 
@@ -110,20 +161,20 @@ hr {
 .total {
     width: 100%;
     font-size: 15px;
-    
+
 }
 
 .total .left {
-  
- float: left;
-   
-    width: 24%;
+
+    float: left;
+
+    width: 34%;
     height: 100%
 }
 
 .total .right {
     float: right;
-    width: 76%;
+    width: 66%;
     height: 100%
 }
 
@@ -165,14 +216,20 @@ hr {
 
 .teacher-image .image-left {
     float: left;
-    width: 16%;
+    width: 15%;
     height: 100%
 }
 
 .teacher-image .image-right {
     float: right;
-    width: 84%;
+    width: 85%;
     height: 100%;
     cursor: pointer;
+    font-size: 13px;
+}
+
+.teacher-image .image-right :hover {
+    color: #1e90ff;
+    font-weight: 400;
 }
 </style>
