286e55fed7728ad62bc67f7896655b6f3fcf0410
王昱璐 <2458651807@qq.com>
2022-09-09 18:17:20 +0800

    科目结业提交

--- src/views/repository/unitSubmit/index.vue
+++ src/views/repository/unitSubmit/index.vue
@@ -2,10 +2,10 @@
     <div class="container">
         <Breadcrumb :items="['题库模块', '结业评价']" />
         <div>
-            <h3>&nbsp;&nbsp;科目结业提交</h3>
+            <h2>&nbsp;&nbsp;科目结业提交</h2>
             <div className="total">
 
-                <p className="left">&nbsp;技术栈:
+                <p className="left">&nbsp;&nbsp;技术栈:
                     <select>
                         <option value="push">下拉菜单</option>
                         <option value="saab">Java</option>
@@ -25,7 +25,7 @@
             </div>
             <hr>
             <div className="second">
-            <p>科目练习项目地址:</p>
+            <p>&nbsp;&nbsp;科目练习项目地址:</p>
                 &nbsp; &nbsp; &nbsp;
                 
                 <a-textarea placeholder="多行输入" style="width:400px;height:80px;margin-left: 130px;margin-top:-30px;" allow-clear/>
@@ -36,7 +36,7 @@
             </div>
             <div className="teacher-grade">
 
-                对当前授课教师评分:
+                &nbsp;&nbsp;对当前授课教师评分:
                 &nbsp;&nbsp;&nbsp;&nbsp;
                 <a-rate :default-value="0" allow-clear style="margin-bottom:-5px;"/>
                 <!--<icon-star size='20px'/>
@@ -55,10 +55,10 @@
             -->
             </div>
             <div className="teacher-image">
-                <div className="image-left">对当前授课老师印象:</div>
+                <div className="image-left">&nbsp;&nbsp;对当前授课老师印象:</div>
                 <div className="image-right">
-                    技术够好&nbsp; 讲课细致&nbsp; 技术一般&nbsp; 讲课卡顿
-                    <p>总爱迟到&nbsp; 认真负责&nbsp; 不关心学员&nbsp; 课程互动多</p>
+                    技术够好&nbsp;&nbsp; 讲课细致&nbsp;&nbsp; 技术一般&nbsp;&nbsp; 讲课卡顿
+                    <p>总爱迟到&nbsp;&nbsp; 认真负责&nbsp;&nbsp; 不关心学员&nbsp;&nbsp; 课程互动多</p>
                 </div>
 
 
@@ -68,16 +68,36 @@
                 &nbsp;&nbsp;&nbsp;其他印象标签(选填):&nbsp;&nbsp;&nbsp;&nbsp;
                 <a-input :style="{ width: '320px' }" placeholder="请填选" allow-clear />
             </div>
-            <button className="button">提交</button>
+            <a-button type="outline" className="button">提交</a-button>
+           
         </div>
     </div>
 </template>
 <script  lang="ts">
+ //import postgetList from './views/unitSubmit.vue';//request.js还没写
 export default {
     name: 'UnitSubmit',
+   
+    data(){
+        return{
+            value:1//postgetList,
+
+        }
+    },
+    methods(){
+
+    },
+    created(){
+        //生命周期钩子
+    }
 };
 </script>
 <style scoped lang="less">
+   .container{
+    background-color: #fff;
+    width:100%;
+ 
+   }
 hr {
     width: 1020px;
 
@@ -88,11 +108,15 @@ hr {
 }
 
 .total {
-    width: 100%
+    width: 100%;
+    font-size: 15px;
+    
 }
 
 .total .left {
-    float: left;
+  
+ float: left;
+   
     width: 24%;
     height: 100%
 }
@@ -106,27 +130,37 @@ hr {
 .second {
     width: 1000px;
     height: 300px;
+    font-size: 15px;
 
 
 }
 
 
 .teacher-grade {
-    /*margin-bottom: 18px;*/
+    margin-bottom: 70px;
+    font-size: 15px;
+
 }
 
 .teacher-image {
     margin-top: 40px;
+    font-size: 15px;
 
 }
 
 .bable {
-    margin-top: 120px;
+    margin-top: 180px;
 }
 
 .button {
-    margin-top: 20px;
+    margin-top: 60px;
     margin-left: 20px;
+    color: #000;
+    border: 1px solid var(--color-neutral-4);
+    width: 70px;
+    height: 25px;
+    border-radius: 5px;
+    font-size: 15px;
 }
 
 .teacher-image .image-left {
