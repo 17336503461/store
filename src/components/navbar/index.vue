c3931568527494a9d0e5337785cb0043427ce91a
ANLX666 <1209322734@qq.com>
2022-09-10 15:17:05 +0800

    a

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -6,7 +6,7 @@
           alt="logo"
           src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image"
         />
-        {{num}}
+        <!-- {{num}} -->
         <a-typography-title
           :style="{ margin: 0, fontSize: '18px' }"
           :heading="5"
