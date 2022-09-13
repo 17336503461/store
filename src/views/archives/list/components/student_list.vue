9046cf64e631d948a9cee9e5cb84a0589c00b1c2
dailongbang <1966249726@qq.com>
2022-09-13 19:40:30 +0800

    学员列表excel的导入与模板下载

--- src/views/archives/list/components/student_list.vue
+++ src/views/archives/list/components/student_list.vue
@@ -7,13 +7,21 @@
         <a-col :span="7">
           <div class="student_search_input">
             <span>学员姓名</span>
-            <a-input v-model="params.realname" class="input" placeholder="请输入学员姓名" />
+            <a-input
+              v-model="params.realname"
+              class="input"
+              placeholder="请输入学员姓名"
+            />
           </div>
         </a-col>
         <a-col :span="7">
           <div class="student_search_input">
             <span>入学时间</span>
-            <a-input v-model="params.enrollmentTimeShool" class="input" placeholder="请输入学员入学时间" />
+            <a-input
+              v-model="params.enrollmentTimeShool"
+              class="input"
+              placeholder="请输入学员入学时间"
+            />
           </div>
         </a-col>
       </a-row>
@@ -22,45 +30,63 @@
         <a-col :span="7">
           <div class="student_search_input">
             <span>学员手机</span>
-            <a-input v-model="params.mobile" class="input" placeholder="请输入学员手机" />
+            <a-input
+              v-model="params.mobile"
+              class="input"
+              placeholder="请输入学员手机"
+            />
           </div>
         </a-col>
         <a-col :span="7">
           <div class="student_search_input">
             <span>所在班级</span>
-            <a-input v-model="params.groupId" class="input" placeholder="请输入学员所在班级" />
+            <a-input
+              v-model="params.groupId"
+              class="input"
+              placeholder="请输入学员所在班级"
+            />
           </div>
         </a-col>
         <a-col :span="8">
-          <a-button class="student_change student_reset" type="dashed" @click="resetFrom"
+          <a-button
+            class="student_change student_reset"
+            type="dashed"
+            @click="resetFrom"
             >重置</a-button
           >
-          <a-button class="student_change" type="dashed" @click="searchStudentFun(params)">查询</a-button>
+          <a-button
+            class="student_change"
+            type="dashed"
+            @click="searchStudentFun(params)"
+            >查询</a-button
+          >
         </a-col>
       </a-row>
     </div>
     <!-- 学员列表区 -->
     <div class="student_list">
-      <a-upload class="uploadStudent" action="https://www.mocky.io/v2/5cc8019d300000980a055e76">
-      </a-upload>
-      <!-- <a-button class="student_entry" type="dashed" @click="addStudentBtn"
-        >录入学员</a-button>
-        <a-button class="student_entry" type="dashed" @click="pluginDownload"
-        >下载录入模板</a-button> -->
+      <div type="primary" class="uploadStudent">
+        <input
+          type="file"
+          class="input_addstudent"
+          accept="application/msexcel"
+          @change="fileChange"
+        />
+      </div>
+      <a-button type="primary" class="download_model">
+        <a href="/addStudentList.xlsx" download="addStudentList.xlsx" style="text-decoration: none;">下载模板</a>
+      </a-button>
+     
+
       <a-table
         class="student_from"
         row-key="id"
         :pagination="pagination"
         :data="studentList"
         :bordered="false"
-    
       >
         <template #columns>
-          <a-table-column
-            :title="$t('ID')"
-            data-index="id"
-            align="center"
-          />
+          <a-table-column :title="$t('ID')" data-index="id" align="center" />
           <a-table-column
             :title="$t('姓名')"
             data-index="realname"
@@ -91,17 +117,27 @@
             data-index="enrollmentTimeShool"
             align="center"
           />
-         
+
           <a-table-column
             :title="$t('操作')"
             data-index="operations"
             align="center"
           >
             <template #cell="row">
-              <a-button v-permission="['admin']" type="text" size="small" @click="editStudentFun(row)">
+              <a-button
+                v-permission="['admin']"
+                type="text"
+                size="small"
+                @click="editStudentFun(row)"
+              >
                 {{ $t('编辑') }}
               </a-button>
-              <a-button v-permission="['admin']"  type="text" size="small" @click="delStudentList(row)">
+              <a-button
+                v-permission="['admin']"
+                type="text"
+                size="small"
+                @click="delStudentList(row)"
+              >
                 {{ $t('删除') }}
               </a-button>
             </template>
@@ -115,225 +151,244 @@
         :visible="visibleEdit"
         title="编辑学员"
         width="640px"
-        
         @ok="handleOk"
         @cancel="handleCancel"
       >
-      <a-form :model="form" :style="{ width: '600px' }">
-      <!-- 输入框（ID） -->
-      <a-form-item field="classname" label="学员ID：">
-        <a-input
-          v-model="form.id"
-          placeholder="请输入学员ID"
-          style="width: 400px"
-        />
-      </a-form-item>
-      <!-- 输入框（姓名） -->
-      <a-form-item field="classname" label="学员姓名：">
-        <a-input
-          v-model="form.realname"
-          placeholder="请输入学员姓名"
-          style="width: 400px"
-        />
-      </a-form-item>
-      <!-- 输入框（学生手机） -->
-      <a-form-item field="classname" label="学员手机：">
-        <a-input
-          v-model="form.mobile"
-          placeholder="请输入学员手机"
-          style="width: 400px"
-        />
-      </a-form-item>
-      <!-- 输入框（学习进度） -->
-      <a-form-item field="classname" label="学习进度：">
-        <a-input
-          v-model="form.groupId"
-          placeholder="请输入学员学习进度"
-          style="width: 400px"
-        />
-      </a-form-item>
-      <!-- 输入框（学生班级） -->
-      <a-form-item field="classname" label="学员班级：">
-        <a-input
-          v-model="form.studentclass"
-          placeholder="请输入学员班级"
-          style="width: 400px"
-        />
-      </a-form-item>
-      <!-- 输入框（学生班级） -->
-      <a-form-item field="classname" label="练习次数：">
-        <a-input
-          v-model="form.seriesId"
-          placeholder="请输入学员练习次数"
-          style="width: 400px"
-        />
-      </a-form-item>
-      <!-- 日期选择器（创建时间） -->
-      <a-form-item field="time" label="入学时间：">
-        <a-date-picker v-model="form.enrollmentTimeShool" style="width: 400px" />
-      </a-form-item>
-      
-    </a-form>
+        <a-form :model="form" :style="{ width: '600px' }">
+          <!-- 输入框（ID） -->
+          <a-form-item field="classname" label="学员ID：">
+            <a-input
+              v-model="form.id"
+              placeholder="请输入学员ID"
+              style="width: 400px"
+            />
+          </a-form-item>
+          <!-- 输入框（姓名） -->
+          <a-form-item field="classname" label="学员姓名：">
+            <a-input
+              v-model="form.realname"
+              placeholder="请输入学员姓名"
+              style="width: 400px"
+            />
+          </a-form-item>
+          <!-- 输入框（学生手机） -->
+          <a-form-item field="classname" label="学员手机：">
+            <a-input
+              v-model="form.mobile"
+              placeholder="请输入学员手机"
+              style="width: 400px"
+            />
+          </a-form-item>
+          <!-- 输入框（学习进度） -->
+          <a-form-item field="classname" label="学习进度：">
+            <a-input
+              v-model="form.groupId"
+              placeholder="请输入学员学习进度"
+              style="width: 400px"
+            />
+          </a-form-item>
+          <!-- 输入框（学生班级） -->
+          <a-form-item field="classname" label="学员班级：">
+            <a-input
+              v-model="form.studentclass"
+              placeholder="请输入学员班级"
+              style="width: 400px"
+            />
+          </a-form-item>
+          <!-- 输入框（学生班级） -->
+          <a-form-item field="classname" label="练习次数：">
+            <a-input
+              v-model="form.seriesId"
+              placeholder="请输入学员练习次数"
+              style="width: 400px"
+            />
+          </a-form-item>
+          <!-- 日期选择器（创建时间） -->
+          <a-form-item field="time" label="入学时间：">
+            <a-date-picker
+              v-model="form.enrollmentTimeShool"
+              style="width: 400px"
+            />
+          </a-form-item>
+        </a-form>
       </a-modal>
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import mitt from '@/utils/mitt';
-  
-  import { getStudentAPI, deleteStudentAPI,addStudentAPI ,updateStudentAPI} from '../../../../api/studentlist'
+  import * as XLSX from 'xlsx';
+  import {
+    getStudentAPI,
+    deleteStudentAPI,
+    addStudentAPI,
+    updateStudentAPI,
+  } from '../../../../api/studentlist';
 
   export default {
-    components: {
-     
-    },
+    components: {},
     data() {
       return {
-         // 表格收集的数据
-            form: {
-              id: '',
-              realname:'',
-              mobile:'',
-              professional:'',
-              groupId:'',
-              seriesId:'',
-              enrollmentTimeShool:'',
-            },
-            // 搜索对象
-            params:{
-              realname:'',
-              enrollmentTimeShool:'',
-              mobile:'',
-              groupId:'',
-            },
-
-            // 学生列表
-            studentList: [
-            ],
-            // 编辑学生对象
-             editStudent: {},            
-            // 分页
-            pagination: {
-              pageSize: 3,
-            },
-            // 录入弹窗
-            
-            visibleEdit: false,
-            
-          }
-    },
-
-  created() {
-    this.acquireStudentList();
-  },
-  
-  methods: {
-    
-    async acquireStudentList() {
-      const res = await getStudentAPI(
-        { pageNum:1,pageSize:3},
-        {
-          'content-type': 'application/json',
-        }
-      );
-      this.studentList = [...res.data.data.list]
-      
-  },
-
-
-   editStudentFun(row) {
-    this.visibleEdit = true;
-    this.form.id = row.record.id
-    this.form.realname = row.record.realname
-    this.form.mobile = row.record.mobile
-    this.form.professional = row.record.professional
-    this.form.groupId = row.record.groupId
-    this.form.seriesId = row.record.seriesId
-    this.form.enrollmentTimeShool = row.record.enrollmentTimeShool
-    },
-    // 确定
-   
-    async handleOk() {
-      const res = await updateStudentAPI({
-        id: this.form.id,
-        realname:this.form.realname,
-        mobile:this.form.mobile,
-        professional:this.form.professional, 
-        groupId:this.form.groupId ,
-        seriesId:this.form.seriesId,
-        enrollmentTimeShool:this.form.enrollmentTimeShool
-      },{
-        'content-type': 'application/json'
-      })
-      this.visibleEdit = false;
-      this.acquireStudentList()
-      
-    },
-    
-    // 取消
-    handleCancel() {
-      this.visibleEdit = false
-    },
-    // 删除按钮
-    async delStudentList(row) {
-      const res = await deleteStudentAPI(row.rowIndex)
-      if(res.data.code !== 200) {
-       this.$message.error('删除失败！')
-      }else{
-        this.$message.success('删除成功!')
-      }
-      this.acquireStudentList()
-     
-      
-    },
-    // 查询操作
-    async searchStudentFun(data) {
-      const res = await getStudentAPI(
-        {
-          pageNum:1,
-          pageSize:3,
-          realname:data.realname,
-          enrollmentTimeShool:data.enrollmentTimeShool,
-          mobile:data.mobile,
-          groupId:data.mobile
+        // 表格收集的数据
+        form: {
+          id: '',
+          realname: '',
+          mobile: '',
+          professional: '',
+          groupId: '',
+          seriesId: '',
+          enrollmentTimeShool: '',
         },
-        {'content-type': 'application/json',})
-      // 搜索成功，清空表单
-      if(res.data.code === 200) {
-        this.resetFrom()
-      }
-      this.studentList = [...res.data.data.list]
-    },
-    // 重置表单
-    resetFrom(){
-      
-      this.params.realname = '';
-      this.params.enrollmentTimeShool = '';
-      this.params.mobile = '';
-      this.params.groupId = '';
-    },
-    // 批量录入
-    addStudentBtn() {
+        // 搜索对象
+        params: {
+          realname: '',
+          enrollmentTimeShool: '',
+          mobile: '',
+          groupId: '',
+        },
 
-    },
-    // 下载模板
-    pluginDownload(){
-     
-    }
-    
-    // 搜索
-    // searchStudentFun(){
-    //   // 输入姓名
-    //   if(this.searchRealname !== ''){
-    //     this.newStudentList = this.studentList.filter((item)=> {
-    //       return item.realname.includes(this.searchRealname)
-    //     })
-    //   }
-    // }
-  },
+        // 学生列表
+        studentList: [],
+        // 编辑学生对象
+        editStudent: {},
+        // 分页
+        pagination: {
+          pageSize: 3,
+        },
+        // 录入弹窗
 
-  }
+        visibleEdit: false,
+      };
+    },
+
+    created() {
+      this.acquireStudentList();
+    },
+
+    methods: {
+      async acquireStudentList() {
+        const res = await getStudentAPI(
+          { pageNum: 1, pageSize: 3 },
+          {
+            'content-type': 'application/json',
+          }
+        );
+        this.studentList = [...res.data.data.list];
+      },
+
+      editStudentFun(row) {
+        this.visibleEdit = true;
+        this.form.id = row.record.id;
+        this.form.realname = row.record.realname;
+        this.form.mobile = row.record.mobile;
+        this.form.professional = row.record.professional;
+        this.form.groupId = row.record.groupId;
+        this.form.seriesId = row.record.seriesId;
+        this.form.enrollmentTimeShool = row.record.enrollmentTimeShool;
+      },
+      // 确定
+      pluginDownload() {
+        window.location.href = '../addStudentList.xlsx';
+      },
+      async handleOk() {
+        const res = await updateStudentAPI(
+          {
+            id: this.form.id,
+            realname: this.form.realname,
+            mobile: this.form.mobile,
+            professional: this.form.professional,
+            groupId: this.form.groupId,
+            seriesId: this.form.seriesId,
+            enrollmentTimeShool: this.form.enrollmentTimeShool,
+          },
+          {
+            'content-type': 'application/json',
+          }
+        );
+        this.visibleEdit = false;
+        this.acquireStudentList();
+      },
+
+      // 取消
+      handleCancel() {
+        this.visibleEdit = false;
+      },
+      // 删除按钮
+      async delStudentList(row) {
+        const res = await deleteStudentAPI(row.rowIndex);
+        if (res.data.code !== 200) {
+          this.$message.error('删除失败！');
+        } else {
+          this.$message.success('删除成功!');
+        }
+        this.acquireStudentList();
+      },
+      // 查询操作
+      async searchStudentFun(data) {
+        const res = await getStudentAPI(
+          {
+            pageNum: 1,
+            pageSize: 3,
+            realname: data.realname,
+            enrollmentTimeShool: data.enrollmentTimeShool,
+            mobile: data.mobile,
+            groupId: data.mobile,
+          },
+          { 'content-type': 'application/json' }
+        );
+        // 搜索成功，清空表单
+        if (res.data.code === 200) {
+          this.resetFrom();
+        }
+        this.studentList = [...res.data.data.list];
+      },
+      // 重置表单
+      resetFrom() {
+        this.params.realname = '';
+        this.params.enrollmentTimeShool = '';
+        this.params.mobile = '';
+        this.params.groupId = '';
+      },
+      fileChange(e) {
+        // file 对象数组
+        console.log(e.target.files);
+        if (e.target.files.length) {
+          const file = e.target.files[0];
+          this.doReadExcelFile(file);
+        }
+      },
+      async doReadExcelFile(file) {
+        this.readFile(file).then((dataBinary) => {
+          const workBook = XLSX.read(dataBinary, {
+            type: 'binary',
+            cellDates: true,
+          });
+          const workSheet = workBook.Sheets[workBook.SheetNames[0]];
+          const data = XLSX.utils.sheet_to_json(workSheet);
+          console.log(data);
+        });
+      },
+      readFile(file) {
+        return new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.readAsBinaryString(file);
+          reader.onload = (ev) => {
+            resolve(ev.target.result);
+          };
+        });
+      },
+
+      // 搜索
+      // searchStudentFun(){
+      //   // 输入姓名
+      //   if(this.searchRealname !== ''){
+      //     this.newStudentList = this.studentList.filter((item)=> {
+      //       return item.realname.includes(this.searchRealname)
+      //     })
+      //   }
+      // }
+    },
+  };
 </script>
 
 <style scoped lang="less">
@@ -412,7 +467,8 @@
     margin: 27px 20px 0 20px;
   }
 
-  .uploadStudent{
+  .uploadStudent {
+    display: block;
     margin: 28px 0 0 19px;
   }
 
@@ -436,12 +492,15 @@
       border-radius: 20px;
     }
   }
-
-  .student_edit {
-    
+  .input_addstudent {
+    border: none;
+  }
+  .download_model {
+    position: absolute;
+    right: 90px;
+    top: 335px;
   }
   .title {
     margin-left: 50px;
   }
-
 </style>
